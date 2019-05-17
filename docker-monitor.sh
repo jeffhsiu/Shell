2866316468f78b8a23482e8be31dbc55a07f751e
Jeff Lin <jeff.lin@zeepin.com>
2019-05-17 13:54:24 +0800

    add memory and cpu monitor

--- docker-monitor.sh
+++ docker-monitor.sh
@@ -2,7 +2,7 @@
 
 ip_addr=`ip a | grep eth0 | grep inet | grep -v inet6 | sed 's/^[ \t]*//g' | cut -d ' ' -f 2 | cut -d '/' -f 1`
 net_limit=20
-mem_limit=60
+mem_limit=50
 cpu_limit=10
 
 wechatSend(){
@@ -18,18 +18,43 @@ do
 	then
 		container_id=$(echo $line | cut -d " " -f 1)
 		name=$(echo $line | cut -d " " -f 2)
+		cpu=$(echo $line | cut -d " " -f 3)
+		mem=$(echo $line | cut -d " " -f 4)
 		net=$(echo $line | cut -d " " -f 8)
+
+		cpu_val=${cpu:0:${#cpu}-1}
+
+		mem_unit=${mem:0-3}
+		mem_val=${mem:0:${#mem}-3}
+
 		net_unit=${net:0-2}
 		net_val=${net:0:${#net}-2}
 
-		echo "ContainerID: $container_id  Name: $name  NetI/O: $net"
+		# CPU 監控
+		if [[ $(echo "${cpu_val} ${cpu_limit}" | awk '{print ($1> $2)}') -eq "1" ]]
+		then
+			echo "CPU使用率過高"
+			title="CPU使用率過高_${ip_addr}"
+			content="VPS IP: ${ip_addr}，Docker Name: ${name}，ContainerID: ${container_id}，CPU %: ${cpu}，Mem: ${mem}，NetIO: ${net}"
+			# wechatSend "${title}" "${content}"
+		fi
 
+		# Memory 監控
+		if [[ $(echo "${mem_val} ${mem_limit}" | awk '{print ($1> $2)}') -eq "1" ]]
+		then
+			echo "Memory使用量過高"
+			title="Memory使用量過高_${ip_addr}"
+			content="VPS IP: ${ip_addr}，Docker Name: ${name}，ContainerID: ${container_id}，CPU %: ${cpu}，Mem: ${mem}，NetIO: ${net}"
+			# wechatSend "${title}" "${content}"
+		fi
+
+		# 網路流量監控
 		if [[ "${net_unit}" == "GB" && $(echo "${net_val} ${net_limit}" | awk '{print ($1> $2)}') -eq "1" ]]
 		then
-			echo "流量超出通知"
-			title="流量超出通知_${ip_addr}"
-			content="VPS IP: ${ip_addr}，ContainerID: ${container_id}，Docker Name: ${name}，NetIO: ${net}，已將該服務暫停"
-			wechatSend "${title}" "${content}"
+			echo "Net流量超出限制"
+			title="Net流量超出通知_${ip_addr}"
+			content="VPS IP: ${ip_addr}，Docker Name: ${name}，ContainerID: ${container_id}，CPU %: ${cpu}，Mem: ${mem}，NetIO: ${net}，已將該服務暫停"
+			# wechatSend "${title}" "${content}"
 		fi
 	fi
 done
