bb204e52a2af0a1d26cec46eecde2bbb9f1207d8
Jeff Lin <jeff.lin@zeepin.com>
2019-05-17 13:18:51 +0800

    debug

--- docker-monitor.sh
+++ docker-monitor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ip_addr=`ip a | grep eth0 | grep inet | grep -v inet6 | sed 's/^[ \t]*//g' | cut -d ' ' -f 2 | cut -d '/' -f 1`
+net_limit=20
+mem_limit=60
+cpu_limit=10
 
 wechatSend(){
 	key="SCU50529T56c1fc580948bf3a0aeed1abfabb93b55ccc07bc5c204"
-	title="$1"
-	content="$2"
 	url="https://sc.ftqq.com/${key}.send"
-	# curl "https://pushbear.ftqq.com/sub?sendkey=${key}&text=${title}&desp=${content}" >/dev/null 2>&1
-	curl -G --data-urlencode "text=${title}" --data-urlencode "desp=${content}" "${url}"
+	curl -G --data-urlencode "text=${1}" --data-urlencode "desp=${2}" "${url}" >/dev/null 2>&1
 }
 
 docker stats --no-stream | while read -r line
@@ -24,10 +24,12 @@ do
 
 		echo "ContainerID: $container_id  Name: $name  NetI/O: $net"
 
-		if [[ "${net_unit}" == "GB" && $(echo "$net_val 20" | awk '{print ($1> $2)}') -eq "1" ]]
+		if [[ "${net_unit}" == "GB" && $(echo "${net_val} ${net_limit}" | awk '{print ($1> $2)}') -eq "1" ]]
 		then
 			echo "流量超出通知"
-			wechatSend "流量超出通知 ${ip_addr}" "VPS IP: ${ip_addr}, Docker Name: ${name}, NetIO: ${net}"
+			title="流量超出通知_${ip_addr}"
+			content="VPS IP: ${ip_addr}，Docker Name: ${name}，NetIO: ${net}，已將該服務暫停"
+			wechatSend ${title} ${content}
 		fi
 	fi
 done
